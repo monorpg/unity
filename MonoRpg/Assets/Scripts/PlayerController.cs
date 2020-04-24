8133397ef0f91255b13509374bc674159f431c1d
Selçuk Ermaya <selcukermaya@gmail.com>
2020-04-24 12:27:02 +0300

    Added basic movements on the map.

---
+++ MonoRpg/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    #region Public
+
+    public LayerMask movementMask;
+    public Interactable focus;
+
+    #endregion Public
+
+    #region Local
+
+    private Camera _cam;
+    private PlayerMotor _motor;
+
+    #endregion Local
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cam = Camera.main;
+        _motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Interact
+        if (Input.GetMouseButton(0))
+        {
+            var ray = _cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out var hit, 100))
+            {
+                var interactable = hit.collider.GetComponent<Interactable>();
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                    _motor.MoveToPoint(interactable.transform.position);
+                }
+            }
+        }
+
+        // Move
+        if (Input.GetMouseButton(1))
+        {
+            var ray = _cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out var hit, 100, movementMask))
+            {
+                _motor.MoveToPoint(hit.point);
+                RemoveFocus();
+            }
+        }
+    }
+
+    private void SetFocus(Interactable newFocus)
+    {
+        if (newFocus != focus)
+        {
+            if (focus != null) focus.OnDefocused();
+
+            focus = newFocus;
+            _motor.FollowTarget(newFocus);
+        }
+
+        newFocus.OnFocused(transform);
+    }
+
+    private void RemoveFocus()
+    {
+        if (focus != null)
+        {
+            focus.OnDefocused();
+        }
+
+        focus = null;
+        _motor.StopFollowingTarget();
+    }
+}
---
+++ MonoRpg/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMotor : MonoBehaviour
+{
+    private Transform target;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+            FaceTarget();
+        }
+    }
+
+    public void MoveToPoint(Vector3 point)
+    {
+        agent.SetDestination(point);
+    }
+
+    public void FollowTarget(Interactable newTarget)
+    {
+        agent.stoppingDistance = newTarget.radius * .8f;
+        agent.updateRotation = false;
+
+        target = newTarget.interactionTransform;
+    }
+
+    public void StopFollowingTarget()
+    {
+        agent.stoppingDistance = 0;
+        agent.updateRotation = true;
+
+        target = null;
+    }
+
+    void FaceTarget()
+    {
+        var direction = (target.position - transform.position).normalized;
+        var lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+}
